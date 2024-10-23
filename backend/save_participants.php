b0fdabb534a4bbe411728571936be1ca29544432
Tilli-simgame <tilli@virtuaali.net>
2024-10-23 19:55:41 +0300

    Added shuffling

--- backend/save_participants.php
+++ backend/save_participants.php
@@ -1,16 +1,21 @@
 <?php
+include 'shuffle_results.php'; 
+
+
 // Get the event name dynamically from the POST request
 $eventName = $_POST['event_name'];
 $riderName = $_POST['rider_name'];
-$email = $_POST['email'];  // Email field if you have it in the form
+//$email = $_POST['email'];  // Email field if you have it in the form
 $horses = $_POST['horse'];  // Array of horses
 $classes = $_POST['class'];  // Array of corresponding classes
 
 // Honeypot bot protection
+/*
 if (!empty($_POST['honeypot'])) {
     echo json_encode(['error' => 'Bot detected!']);
     exit;
 }
+    */
 
 // Define the JSON file path based on the event name
 $participantsFile = "../data/$eventName.json";
@@ -51,31 +56,11 @@ foreach ($classes as $index => $className) {
         'horse' => $horseName
     ];
 
-    // Check if the class is now full after adding the participant
-    if (count($participantsData['classes'][$className]) == $rules['maxHorsesPerClass']) {
-        // Class is full, generate results
-        $participants = $participantsData['classes'][$className];
-        
-        // Randomize the results
-        shuffle($participants);
-        
-        // Assign places (e.g., 1st, 2nd, 3rd, etc.)
-        $results = [];
-        foreach ($participants as $place => $participant) {
-            $results[] = [
-                'place' => $place + 1,
-                'rider' => $participant['rider'],
-                'horse' => $participant['horse']
-            ];
-        }
-        
-        // Save the results in the JSON file under "results"
-        if (!isset($participantsData['results'])) {
-            $participantsData['results'] = [];
-        }
-        
-        $participantsData['results'][$className] = $results;
-    }
+  // If class is full, shuffle and generate results
+  if (count($participantsData['classes'][$className]) == $rules['maxHorsesPerClass']) {
+    $participants = $participantsData['classes'][$className];
+    $participantsData['results'][$className] = shuffleResults($participants);
+}
 }
 
 // Save the updated data back to the JSON file
