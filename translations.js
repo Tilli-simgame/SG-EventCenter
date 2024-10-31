04032796fdb074d9c623a25fc3e843168e6e1fea
Thilithiel Aéthelwyn "Tilli <135822504+Tilli-simgame@users.noreply.github.com>
2024-10-31 18:55:14 +0200

    Added language selector

---
+++ translations.js
@@ -0,0 +1,44 @@
+// translations.js
+
+const translations = {
+    en: {
+        title: "Dressage Event Registration for ${date}",
+        eventDate: "Event Date: ${date}",
+        lastEntryDate: "Last Day to Enter: ${lastDate}",
+        organizer: "Organizer: ${organizer}",
+        email: "Email: <a href='mailto:${email}'>${email}</a>",
+        association: "Association: ${association}",
+        location: "Location: <a href='https://${url}' target='_blank'>${location}</a>",
+        eventType: "Event Type: ${type}",
+        maxHorsesPerClass: "Max Horses per Class: ${maxHorsesPerClass}",
+        maxClassesPerHorse: "Max Classes per Horse: ${maxClassesPerHorse}",
+        maxHorsesPerRiderInClass: "Max Horses per Rider in Class: ${maxHorsesPerRiderInClass}",
+        eventInfoTitle: "Event Information",
+        eventRulesTitle: "Event Rules",
+        registerForEvent: "Register for the Event",
+        ridersName: "Rider's Name:",
+        submitButton: "Submit",
+        participantsResultsTitle: "Participants and Results",
+        placeholderHorseName: "Horse's Name"
+    },
+    fi: {
+        title: "Kouluratsastustapahtuman Ilmoittautuminen ${date}",
+        eventDate: "Tapahtuman Päivämäärä: ${date}",
+        lastEntryDate: "Viimeinen Ilmoittautumispäivä: ${lastDate}",
+        organizer: "Järjestäjä: ${organizer}",
+        email: "Sähköposti: <a href='mailto:${email}'>${email}</a>",
+        association: "Yhdistys: ${association}",
+        location: "Sijainti: <a href='https://${url}' target='_blank'>${location}</a>",
+        eventType: "Tapahtumatyyppi: ${type}",
+        maxHorsesPerClass: "Maksimi Hevosia per Luokka: ${maxHorsesPerClass}",
+        maxClassesPerHorse: "Maksimi Luokkia per Hevonen: ${maxClassesPerHorse}",
+        maxHorsesPerRiderInClass: "Maksimi Hevosia per Ratsastaja Luokassa: ${maxHorsesPerRiderInClass}",
+        eventInfoTitle: "Tapahtumatiedot",
+        eventRulesTitle: "Säännöt",
+        registerForEvent: "Ilmoittaudu Tapahtumaan",
+        ridersName: "Ratsastajan Nimi:",
+        submitButton: "Lähetä",
+        participantsResultsTitle: "Osallistujat ja Tulokset",
+        placeholderHorseName: "Hevosen nimi"
+    }
+};
