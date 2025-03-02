ac1b6b7a7c307d93a24e3404ead9aecadce35d27
Tilli-simgame <tilli@virtuaali.net>
2025-03-02 11:25:01 +0200

    Updated

--- config.js
+++ config.js
@@ -8,6 +8,5 @@ const CONFIG = {
     BASE_URL: "__PRODUCTION_BASE_URL__" // Placeholder to be replaced in GitHub Actions
   }
 };
-
 // Set API_URL based on the current environment
 window.API_URL = CONFIG[ENV].BASE_URL;
